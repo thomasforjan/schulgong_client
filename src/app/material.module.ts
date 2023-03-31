3f004514a38c2b67a5d969517681904b9e8b69c7
Philipp Wildzeiss <philipp.wildzeiss@aon.at>
2023-03-31 23:34:28 +0200

    add: Setup Angular application
    Setup Angular application v15.2.0.
    Add Angular Material v15.2.5.
    Setup project structure.
    
    SCHUL-15

---
+++ src/app/material.module.ts
@@ -0,0 +1,13 @@
+import { NgModule } from "@angular/core";
+
+import { MatButtonModule } from "@angular/material/button";
+
+const material = [
+    MatButtonModule 
+];
+
+@NgModule({
+    imports: [...material],
+    exports: [material],
+})
+export class MaterialModule{}
