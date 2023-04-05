fa721299d10a4e44c2c1943c570b9a7790fb4749
Martin Kral <2110859031@fh-burgenland.at>
2023-04-05 14:40:15 +0200

    Add: TestComponent to test, if database connection can be established
    SCHUL-17

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,16 +5,21 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { MaterialModule } from './material.module';
+import {TestComponent} from "./test/test.component";
+import {HttpClientModule} from "@angular/common/http";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TestComponent
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
-    MaterialModule
+    MaterialModule,
+    HttpClientModule
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/test/store.service.ts
@@ -0,0 +1,19 @@
+import {Injectable} from "@angular/core";
+
+/**
+ * @author Thomas Forjan, Philipp Wildzeiss, Martin Kral
+ * @version 0.1
+ * @implNote practical project
+ * @since March 2023
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class StoreService{
+  public test: any[] = [];
+
+
+
+  constructor() {
+  }
+}
