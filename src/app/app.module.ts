11e392a81bb15897322126ef503d956934c96a81
Raylee <rayleesproject@gmail.com>
2023-04-12 22:18:19 +0200

    add: Footer component
    The Footer component for the layout was implemented.
    
    SCHUL-24

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,27 +1,52 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
 import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
+
+/**
+ * Material Module
+ */
 import { MaterialModule } from './material.module';
-import {TestComponent} from "./test/test.component";
-import {HttpClientModule} from "@angular/common/http";
+
+/**
+ * Components
+ */
+import { AppComponent } from './app.component';
+import { GridCardsComponent } from './components/grid-cards/grid-cards.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { InfoBarComponent } from './components/info-bar/info-bar.component';
+import { ButtonComponent } from './components/button/button.component';
+import { HeroImageComponent } from './components/hero-image/hero-image.component';
+
+/**
+ * Test Components
+ */
+import { TestComponent } from './test/test.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    TestComponent
+    HeaderComponent,
+    SidebarComponent,
+    FooterComponent,
+    GridCardsComponent,
+    InfoBarComponent,
+    ButtonComponent,
+    HeroImageComponent,
+
+    TestComponent, // Test Component
   ],
   imports: [
     BrowserModule,
     AppRoutingModule,
     BrowserAnimationsModule,
     MaterialModule,
-    HttpClientModule
-
+    HttpClientModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/layout/footer/footer.component.ts
@@ -0,0 +1,14 @@
+/**
+- author: Thomas Forjan, Philipp Wildzeiss, Martin Kral
+- version: 0.0.1
+- date: 31.03.2023
+- description: Footer component
+*/
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-footer',
+  templateUrl: './footer.component.html',
+  styleUrls: ['./footer.component.scss'],
+})
+export class FooterComponent {}
