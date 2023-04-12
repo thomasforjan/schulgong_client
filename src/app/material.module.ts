8850d3f60f8e7f6f99a69131371ef23d17301baf
Raylee <rayleesproject@gmail.com>
2023-04-12 22:14:30 +0200

    update: Material module with all needed Angular material components
    Added all the required Angular Material components needed for the layout.
    
    SCHUL-24

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,13 +1,33 @@
-import { NgModule } from "@angular/core";
+/**
+- author: Thomas Forjan, Philipp Wildzeiss, Martin Kral
+- version: 0.0.1
+- date: 31.03.2023
+- description: Material module
+*/
 
-import { MatButtonModule } from "@angular/material/button";
+import { NgModule } from '@angular/core';
 
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatCardModule } from '@angular/material/card';
+import { MatButtonModule } from '@angular/material/button';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatMenuModule } from '@angular/material/menu';
+
+/**
+ * Angular Material components
+ */
 const material = [
-    MatButtonModule 
+  MatToolbarModule,
+  MatIconModule,
+  MatCardModule,
+  MatButtonModule,
+  MatTooltipModule,
+  MatMenuModule,
 ];
 
 @NgModule({
-    imports: [...material],
-    exports: [material],
+  imports: [...material],
+  exports: [material],
 })
-export class MaterialModule{}
+export class MaterialModule {}
