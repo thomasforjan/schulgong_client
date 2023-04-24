e133e824ddedb1adf86d8b507cd494d4dc03086c
Martin Kral <2110859031@fh-burgenland.at>
2023-04-24 02:15:30 +0200

    add: Ringtone modules
    Add ringtone modules and delete placeholder

---
+++ src/app/models/Ringtone.ts
@@ -0,0 +1,28 @@
+/**
+ - author: Thomas Forjan, Philipp Wildzeiss, Martin Kral
+ - version: 0.0.1
+ - date: April 2023
+ - description: Models for ringtones
+ */
+export interface Ringtone {
+  id: number;
+  name: string;
+  filename: string;
+  path: string;
+  date: string;
+  size: number;
+}
+
+export interface RingtonePayload {
+  name: string;
+  filename: string;
+  path: string;
+  date: Date;
+  size: number;
+}
+
+export interface RingtoneResponse {
+  _embedded: {
+    ringtoneDTOList: Ringtone[];
+  };
+}
