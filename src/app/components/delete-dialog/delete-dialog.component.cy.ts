26b4adfd6a504465d0ac2effc1a5b2f3e49eadeb
Martin Kral <2110859031@fh-burgenland.at>
2023-05-12 13:30:58 +0200

    refactor: Delete component
    Refactored delete component and added jsdoc.

--- src/app/components/delete-dialog/delete-dialog.component.cy.ts
+++ src/app/components/delete-dialog/delete-dialog.component.cy.ts
@@ -1,5 +1,11 @@
 import { DeleteDialogComponent } from './delete-dialog.component'
 
+/**
+ * @author: Thomas Forjan, Philipp Wildzeiss, Martin Kral
+ * @version: 0.0.2
+ * @since: April 2023
+ * @description: Test cases for Reusable delete dialog
+ */
 describe('DeleteDialogComponent', () => {
   it('should mount', () => {
     // cy.mount(DeleteDialogComponent)
--- src/app/components/delete-dialog/delete-dialog.component.spec.ts
+++ src/app/components/delete-dialog/delete-dialog.component.spec.ts
@@ -1,16 +1,29 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { DeleteDialogComponent } from './delete-dialog.component';
+import {DeleteDialogComponent} from './delete-dialog.component';
+import {HeroImageComponent} from "../hero-image/hero-image.component";
+import {ButtonComponent} from "../button/button.component";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatIconModule} from "@angular/material/icon";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
 
+/**
+ * @author: Thomas Forjan, Philipp Wildzeiss, Martin Kral
+ * @version: 0.0.2
+ * @since: April 2023
+ * @description: Tests for reusable delete dialog
+ */
 describe('DeleteDialogComponent', () => {
   let component: DeleteDialogComponent;
   let fixture: ComponentFixture<DeleteDialogComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ DeleteDialogComponent ]
+      declarations: [DeleteDialogComponent, HeroImageComponent, ButtonComponent ],
+      imports: [MatTooltipModule, MatIconModule],
+      providers: [{provide: MatDialogRef, useValue: {}}, {provide: MAT_DIALOG_DATA, useValue: {}}]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(DeleteDialogComponent);
     component = fixture.componentInstance;
