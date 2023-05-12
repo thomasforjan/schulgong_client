3d5ed795d089e958ec67fcff088571d5c8695758
Martin Kral <2110859031@fh-burgenland.at>
2023-05-12 13:29:53 +0200

    refactor: Button component
    Refactored jsdoc and changed private variables from variable to _variable

--- src/app/components/button/button.component.spec.ts
+++ src/app/components/button/button.component.spec.ts
@@ -1,7 +1,14 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { ButtonComponent } from './button.component';
+import {MatTooltipModule} from "@angular/material/tooltip";
 
+/**
+ * @author: Thomas Forjan, Philipp Wildzeiss, Martin Kral
+ * @version: 0.0.2
+ * @since: April 2023
+ * @description: Testcases for reusable button component
+ */
 describe('ButtonComponent', () => {
   let component: ButtonComponent;
   let fixture: ComponentFixture<ButtonComponent>;
@@ -9,6 +16,7 @@ describe('ButtonComponent', () => {
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       declarations: [ButtonComponent],
+      imports: [MatTooltipModule]
     }).compileComponents();
 
     fixture = TestBed.createComponent(ButtonComponent);
