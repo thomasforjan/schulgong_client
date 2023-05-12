e017342c969e9b5b34bee4a98060b85ec563e38b
Martin Kral <2110859031@fh-burgenland.at>
2023-05-12 13:39:15 +0200

    refactor: Add-edit-ringtime component
    Refactored code and updated jsdoc

--- src/app/pages/ringtime/add-edit-ringtime/add-edit-ringtime.component.spec.ts
+++ src/app/pages/ringtime/add-edit-ringtime/add-edit-ringtime.component.spec.ts
@@ -1,16 +1,40 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { AddEditRingtimeComponent } from './add-edit-ringtime.component';
+import {AddEditRingtimeComponent} from './add-edit-ringtime.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {HeroImageComponent} from "../../../components/hero-image/hero-image.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatNativeDateModule} from "@angular/material/core";
+import {ButtonComponent} from "../../../components/button/button.component";
+import {MatCheckboxModule} from "@angular/material/checkbox";
+import {MatSelectModule} from "@angular/material/select";
+import {MatInputModule} from "@angular/material/input";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatIconModule} from "@angular/material/icon";
 
+/**
+ * @author: Thomas Forjan, Philipp Wildzeiss, Martin Kral
+ * @version: 0.0.2
+ * @since: April 2023
+ * @description: Tests for the add-edit-ringtime component
+ */
 describe('AddEditRingtimeComponent', () => {
   let component: AddEditRingtimeComponent;
   let fixture: ComponentFixture<AddEditRingtimeComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AddEditRingtimeComponent ]
+      declarations: [AddEditRingtimeComponent, HeroImageComponent, ButtonComponent],
+      imports: [HttpClientTestingModule, ReactiveFormsModule, MatFormFieldModule, MatDatepickerModule,
+        MatNativeDateModule, MatCheckboxModule, MatSelectModule, MatInputModule, BrowserAnimationsModule, MatTooltipModule,
+        MatIconModule],
+      providers: [{provide: MatDialogRef, useValue: {}}, {provide: MAT_DIALOG_DATA, useValue: {}}]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(AddEditRingtimeComponent);
     component = fixture.componentInstance;
--- src/app/pages/ringtime/add-edit-ringtime/add-edit-ringtime.component.ts
+++ src/app/pages/ringtime/add-edit-ringtime/add-edit-ringtime.component.ts
@@ -1,23 +1,23 @@
 import {Component, Inject, OnInit} from '@angular/core';
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {BackendService} from "../../../services/backend.service";
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
-import {Ringtime, RingtimeDialog} from "../../../models/Ringtime";
-import {HeroImages, StoreService} from "../../../services/store.service";
-import {map, tap} from "rxjs";
+import {FormControl, FormGroup, Validators} from '@angular/forms';
+import {MAT_DIALOG_DATA, MatDialogRef} from '@angular/material/dialog';
+import {RingtimeDialog} from '../../../models/Ringtime';
+import {HeroImages, StoreService} from '../../../services/store.service';
+import {DateUtilsService} from 'src/app/services/date-utils.service';
+import {RingtoneBackendService} from "../../../services/ringtone.backend.service";
 
 /**
- - author: Thomas Forjan, Philipp Wildzeiss, Martin Kral
- - version: 0.0.1
- - date: 12.04.2023
- - description: Ringtime component
+ * @author: Thomas Forjan, Philipp Wildzeiss, Martin Kral
+ * @version: 0.0.2
+ * @since: April 2023
+ * @description: Add-edit-ringtime component
  */
 @Component({
   selector: 'app-add-edit-ringtime',
   templateUrl: './add-edit-ringtime.component.html',
-  styleUrls: ['./add-edit-ringtime.component.scss']
+  styleUrls: ['./add-edit-ringtime.component.scss'],
 })
-export class AddEditRingtimeComponent implements OnInit{
+export class AddEditRingtimeComponent implements OnInit {
   /**
    * Formgroup of ringtimeForm
    */
@@ -26,87 +26,104 @@ export class AddEditRingtimeComponent implements OnInit{
   /**
    * Boolean to check if the user wants to edit a ringtime
    */
-  public isEditRingtime = false;
+  isEditRingtime = false;
 
   /**
-   * Boolean to check if the user wants to add a new ringtime
+   * Ringtime Hero image from enum in store service
    */
-  public isAddRingtime = false;
-
-  /**
-   * Dashboard icons from enum in store service
-   */
-  ringtimeIcon: string[] = Object.values(HeroImages);
+  ringtimeHeroImage: string = HeroImages.RingtimeHeroImage;
 
   constructor(
-    private _backendService: BackendService,
-    private _formBuilder: FormBuilder,
+    private _ringtoneBackendService: RingtoneBackendService,
+    public dateUtilsService: DateUtilsService,
     public dialogRef: MatDialogRef<AddEditRingtimeComponent>,
     public storeService: StoreService,
     @Inject(MAT_DIALOG_DATA)
-    public data: {isAddRingtime: boolean; ringtimeDialog: RingtimeDialog; index: string},
-  ) {
-    if (this.data) {
-      this.isAddRingtime = data.isAddRingtime;
-      this.isEditRingtime = !data.isAddRingtime;
+    public data: {
+      isAddRingtime: boolean;
+      ringtimeDialog: RingtimeDialog;
+      index: string;
     }
+  ) {
   }
 
   ngOnInit() {
     this.loadRingtones();
-
-    if (this.data) {
-      this.isEditRingtime = true;
-      this.ringtimeForm = this._formBuilder.group({
-        id: [this.data.ringtimeDialog.id],
-        name: [this.data.ringtimeDialog.name],
-        startDate: [this.convertDateTimeToString(this.data.ringtimeDialog.startDate, 'en-CA'), Validators.required],
-        endDate: [this.convertDateTimeToString(this.data.ringtimeDialog.endDate, 'en-CA')],
-        monday: [this.data.ringtimeDialog.monday],
-        tuesday: [this.data.ringtimeDialog.tuesday],
-        wednesday: [this.data.ringtimeDialog.wednesday],
-        thursday: [this.data.ringtimeDialog.thursday],
-        friday: [this.data.ringtimeDialog.friday],
-        saturday: [this.data.ringtimeDialog.saturday],
-        sunday: [this.data.ringtimeDialog.sunday],
-        playTime: [this.data.ringtimeDialog.playTime, Validators.required],
-        ringtoneId: [this.data.ringtimeDialog.ringtoneId, Validators.required]
-      });
-    } else {
-      this.isAddRingtime = true;
-      this.ringtimeForm = this._formBuilder.group({
-        name: ['', Validators.required],
-        startDate: ['', Validators.required],
-        endDate: [''],
-        monday: [''],
-        tuesday: [''],
-        wednesday: [''],
-        thursday: [''],
-        friday: [''],
-        saturday: [''],
-        sunday: [''],
-        playTime: ['', Validators.required],
-        ringtoneId: ['', Validators.required]
-      });
-    }
+    this.isEditRingtime = !this.data?.isAddRingtime;
+    this.ringtimeForm = new FormGroup(
+      {
+        id: new FormControl(this.data?.ringtimeDialog?.id),
+        name: new FormControl(
+          this.data?.ringtimeDialog?.name ?? '',
+          Validators.required
+        ),
+        startDate: new FormControl(
+          this.data?.ringtimeDialog
+            ? this.dateUtilsService.convertDateTimeToString(
+              this.data.ringtimeDialog.startDate,
+              'en-CA'
+            )
+            : '',
+          Validators.required
+        ),
+        endDate: new FormControl(
+          this.data?.ringtimeDialog
+            ? this.dateUtilsService.convertDateTimeToString(
+              this.data.ringtimeDialog.endDate,
+              'en-CA'
+            )
+            : '',
+          Validators.required
+        ),
+        monday: new FormControl(this.data?.ringtimeDialog?.monday ?? ''),
+        tuesday: new FormControl(this.data?.ringtimeDialog?.tuesday ?? ''),
+        wednesday: new FormControl(this.data?.ringtimeDialog?.wednesday ?? ''),
+        thursday: new FormControl(this.data?.ringtimeDialog?.thursday ?? ''),
+        friday: new FormControl(this.data?.ringtimeDialog?.friday ?? ''),
+        saturday: new FormControl(this.data?.ringtimeDialog?.saturday ?? ''),
+        sunday: new FormControl(this.data?.ringtimeDialog?.sunday ?? ''),
+        playTime: new FormControl(
+          this.data?.ringtimeDialog?.playTime ?? '',
+          Validators.required
+        ),
+        ringtoneId: new FormControl(
+          this.data?.ringtimeDialog?.ringtoneId ?? '',
+          Validators.required
+        ),
+      },
+      {validators: this.dateUtilsService.dateRangeValidator}
+    );
   }
 
   /**
    * Load ringtones from the database
    */
   loadRingtones() {
-    this._backendService.getRingtoneResponse()
+    this._ringtoneBackendService.getRingtoneResponse();
   }
 
   /**
    * Method which is called when the submit button is clicked
    */
   onSubmitClick() {
-    if(this.ringtimeForm?.valid) {
+    if (this.ringtimeForm?.valid) {
       const startDate = new Date(this.ringtimeForm.value.startDate);
       const endDate = new Date(this.ringtimeForm.value.endDate);
-      this.ringtimeForm.value.startDate = this.convertDateTimeToString(startDate, 'en-CA');
-      this.ringtimeForm.value.endDate = this.convertDateTimeToString(endDate, 'en-CA');
+      this.ringtimeForm.value.startDate =
+        this.dateUtilsService.convertDateTimeToString(startDate, 'en-CA');
+      this.ringtimeForm.value.endDate =
+        this.dateUtilsService.convertDateTimeToString(endDate, 'en-CA');
+
+      // Check if end date is greater or equal than start date
+      const errors = this.dateUtilsService.dateRangeValidator(
+        this.ringtimeForm
+      );
+      if (errors) {
+        this.ringtimeForm.setErrors(errors);
+        return;
+      }
+
+      // If no errors, close dialog
       this.dialogRef.close(this.ringtimeForm.value);
     }
   }
@@ -117,38 +134,4 @@ export class AddEditRingtimeComponent implements OnInit{
   onCancelClick() {
     this.dialogRef.close();
   }
-
-  /**
-   * Method which is called when the start date is changed
-   */
-  startDateChange(event: any) {
-    this.ringtimeForm.value.startDate = event.target.value;
-  }
-
-  /**
-   * Method which is called when the end date is changed
-   */
-  endDateChange(event: any) {
-    this.ringtimeForm.value.endDate = event.target.value;
-  }
-
-  /**
-   * Method which is called when the playtime is changed
-   */
-  playTimeChange(event: any) {
-    this.ringtimeForm.value.playTime = event.target.value;
-  }
-
-  /**
-   * Converts date time to string
-   */
-  convertDateTimeToString(date: Date, locales: string): string {
-    let dateString = new Date(date).toLocaleDateString(locales, {
-      day: '2-digit',
-      month: '2-digit',
-      year: 'numeric'
-    });
-    return dateString;
-  }
-
 }
