a02892374d7f63a98fba67be886cb2bb9e6fae1d
Martin Kral <2110859031@fh-burgenland.at>
2023-05-12 13:34:01 +0200

    refactor: Footer component
    Refactored footer component and updated jsdoc

--- src/app/layout/footer/footer.component.spec.ts
+++ src/app/layout/footer/footer.component.spec.ts
@@ -1,16 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { FooterComponent } from './footer.component';
+import {FooterComponent} from './footer.component';
 
+/**
+ * @author: Thomas Forjan, Philipp Wildzeiss, Martin Kral
+ * @version: 0.0.2
+ * @since: April 2023
+ * @description: Tests for the footer component
+ */
 describe('FooterComponent', () => {
   let component: FooterComponent;
   let fixture: ComponentFixture<FooterComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ FooterComponent ]
+      declarations: [FooterComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(FooterComponent);
     component = fixture.componentInstance;
