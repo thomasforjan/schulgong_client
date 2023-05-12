e54742d19ea3424e534e6f69a0c2339b8af2d228
Martin Kral <2110859031@fh-burgenland.at>
2023-05-12 13:40:29 +0200

    refactor: Ringtones component
    Refactored code and updated jsdoc

--- src/app/pages/ringtones/ringtones.component.spec.ts
+++ src/app/pages/ringtones/ringtones.component.spec.ts
@@ -1,16 +1,31 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { RingtonesComponent } from './ringtones.component';
+import {RingtonesComponent} from './ringtones.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {MatDialogModule} from "@angular/material/dialog";
+import {MatSnackBarModule} from "@angular/material/snack-bar";
+import {HeroImageComponent} from "../../components/hero-image/hero-image.component";
+import {ButtonComponent} from "../../components/button/button.component";
+import {GridCardsComponent} from "../../components/grid-cards/grid-cards.component";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatIconModule} from "@angular/material/icon";
 
+/**
+ * @author: Thomas Forjan, Philipp Wildzeiss, Martin Kral
+ * @version: 0.0.2
+ * @since: April 2023
+ * @description: Tests for the ringtone component
+ */
 describe('RingtonesComponent', () => {
   let component: RingtonesComponent;
   let fixture: ComponentFixture<RingtonesComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ RingtonesComponent ]
+      declarations: [RingtonesComponent, HeroImageComponent, ButtonComponent, GridCardsComponent],
+      imports: [HttpClientTestingModule, MatDialogModule, MatSnackBarModule, MatTooltipModule, MatIconModule]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(RingtonesComponent);
     component = fixture.componentInstance;
