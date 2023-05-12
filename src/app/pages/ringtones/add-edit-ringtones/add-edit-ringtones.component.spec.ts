628291bdfd21746769da8b924378f6d24effad61
Martin Kral <2110859031@fh-burgenland.at>
2023-05-12 13:40:10 +0200

    refactor: Add-edit-ringtones component
    Refactored code and updated jsdoc

--- src/app/pages/ringtones/add-edit-ringtones/add-edit-ringtones.component.spec.ts
+++ src/app/pages/ringtones/add-edit-ringtones/add-edit-ringtones.component.spec.ts
@@ -1,16 +1,33 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { AddEditRingtonesComponent } from './add-edit-ringtones.component';
+import {AddEditRingtonesComponent} from './add-edit-ringtones.component';
+import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import {HeroImageComponent} from "../../../components/hero-image/hero-image.component";
+import {MatFormFieldModule} from "@angular/material/form-field";
+import {ButtonComponent} from "../../../components/button/button.component";
+import {ReactiveFormsModule} from "@angular/forms";
+import {MatInputModule} from "@angular/material/input";
+import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {MatTooltipModule} from "@angular/material/tooltip";
+import {MatIconModule} from "@angular/material/icon";
 
+/**
+ * @author: Thomas Forjan, Philipp Wildzeiss, Martin Kral
+ * @version: 0.0.2
+ * @since: April 2023
+ * @description: Tests for the add-edit-ringtone component
+ */
 describe('AddEditRingtonesComponent', () => {
   let component: AddEditRingtonesComponent;
   let fixture: ComponentFixture<AddEditRingtonesComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ AddEditRingtonesComponent ]
+      declarations: [AddEditRingtonesComponent, HeroImageComponent, ButtonComponent],
+      imports: [MatFormFieldModule, ReactiveFormsModule, MatInputModule, BrowserAnimationsModule, MatTooltipModule, MatIconModule],
+      providers: [{provide: MatDialogRef, useValue: {}}, {provide: MAT_DIALOG_DATA, useValue: {}}]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(AddEditRingtonesComponent);
     component = fixture.componentInstance;
