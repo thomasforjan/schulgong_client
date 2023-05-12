d78fcfacab2a2fda9e2407c4c7f64bb9ee14c098
Martin Kral <2110859031@fh-burgenland.at>
2023-05-12 13:36:17 +0200

    refactor: Sidebar component
    Refactored sidebar component, updated jsdoc and changed private variables into _variables

--- src/app/layout/sidebar/sidebar.component.spec.ts
+++ src/app/layout/sidebar/sidebar.component.spec.ts
@@ -1,16 +1,24 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { SidebarComponent } from './sidebar.component';
+import {SidebarComponent} from './sidebar.component';
+import {AppRoutingModule} from "../../app-routing.module";
 
+/**
+ * @author: Thomas Forjan, Philipp Wildzeiss, Martin Kral
+ * @version: 0.0.2
+ * @since: April 2023
+ * @description: Tests for the sidebar component
+ */
 describe('SidebarComponent', () => {
   let component: SidebarComponent;
   let fixture: ComponentFixture<SidebarComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ SidebarComponent ]
+      declarations: [SidebarComponent],
+      imports: [AppRoutingModule]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(SidebarComponent);
     component = fixture.componentInstance;
