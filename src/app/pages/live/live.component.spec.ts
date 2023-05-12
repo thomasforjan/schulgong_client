1f2235ea38b808d5750ca4bfd6fd5e4480ae9312
Martin Kral <2110859031@fh-burgenland.at>
2023-05-12 13:38:04 +0200

    refactor: Live component
    Updated jsdoc

--- src/app/pages/live/live.component.spec.ts
+++ src/app/pages/live/live.component.spec.ts
@@ -1,16 +1,22 @@
-import { ComponentFixture, TestBed } from '@angular/core/testing';
+import {ComponentFixture, TestBed} from '@angular/core/testing';
 
-import { LiveComponent } from './live.component';
+import {LiveComponent} from './live.component';
 
+/**
+ * @author: Thomas Forjan, Philipp Wildzeiss, Martin Kral
+ * @version: 0.0.2
+ * @since: May 2023
+ * @description: Tests for the live component
+ */
 describe('LiveComponent', () => {
   let component: LiveComponent;
   let fixture: ComponentFixture<LiveComponent>;
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LiveComponent ]
+      declarations: [LiveComponent]
     })
-    .compileComponents();
+      .compileComponents();
 
     fixture = TestBed.createComponent(LiveComponent);
     component = fixture.componentInstance;
