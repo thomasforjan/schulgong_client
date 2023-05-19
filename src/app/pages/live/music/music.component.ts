bc3c906a12ac7bbb0f064d9d6426ce4957490eb0
Thomas Forjan <112717699+thomasforjan@users.noreply.github.com>
2023-05-19 21:53:43 +0200

    add: Music component implementation notes

--- src/app/pages/live/music/music.component.ts
+++ src/app/pages/live/music/music.component.ts
@@ -1,5 +1,11 @@
 import { Component } from '@angular/core';
 
+/**
+ * @author: Thomas Forjan, Philipp Wildzeiss, Martin Kral
+ * @version: 0.0.1
+ * @since: May 2023
+ * @description: Music component
+ */
 @Component({
   selector: 'app-music',
   templateUrl: './music.component.html',
