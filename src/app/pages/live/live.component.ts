57464a8f32cdaeb6d3f5bc5d2d3e156d74eb3891
Thomas Forjan <112717699+thomasforjan@users.noreply.github.com>
2023-05-19 12:03:28 +0200

    add: Functionality in live.component.html and live.component.ts
    Added functionality in live.component.html
    
    Added functionality in live.component.ts:
    - Defined Live Hero Image from enum in store service.
    - Defined Router links from enum in store service.
    - Defined Live icons from enum in store service.
    - Defined Live title from enum in store service.
    - Added a helper variable to store the original title.
    - Added a boolean to check if recording is in enabled state.
    - Added a boolean to check if new recording is allowed.
    - Added a boolean to check if recording controls are visible.
    - Added a boolean to check if alarm is enabled.
    - Implemented onRecordToggle method to toggle recording status.
    - Implemented onPlay method to play the recording.
    - Implemented onSend method to send the recording.
    - Implemented onDelete method to delete the recording.
    - Implemented resetRecordingState method to reset the recording state.
    - Implemented onAlarmToggle method to toggle the alarm state.
    
    Added live.component.scss styles.
    
    SCHUL-27

--- src/app/pages/live/live.component.ts
+++ src/app/pages/live/live.component.ts
@@ -1,4 +1,10 @@
-import {Component} from '@angular/core';
+import { Component } from '@angular/core';
+import {
+  HeroImages,
+  LiveIcons,
+  LiveNames,
+  RoutingLinks,
+} from 'src/app/services/store.service';
 
 /**
  * @author: Thomas Forjan, Philipp Wildzeiss, Martin Kral
@@ -9,8 +15,141 @@ import {Component} from '@angular/core';
 @Component({
   selector: 'app-live',
   templateUrl: './live.component.html',
-  styleUrls: ['./live.component.scss']
+  styleUrls: ['./live.component.scss'],
 })
 export class LiveComponent {
+  /**
+   * Live Hero Image from enum in store service
+   */
+  liveHeroImage: string = HeroImages.LiveHeroImage;
 
+  /**
+   * Router links from enum in store service
+   */
+  routerLinks: string[] = Object.values(RoutingLinks);
+
+  /**
+   * Live icons from enum in store service
+   */
+  liveIcons: string[] = Object.values(LiveIcons);
+
+  /**
+   * Live title from enum in store service
+   */
+  liveTitles: string[] = Object.values(LiveNames);
+
+  /**
+   * Helper variable to store original title
+   */
+  originalTitle: string = '';
+
+  /**
+   * Boolean to check if recording is in enabled state
+   */
+  isRecording: boolean = false;
+
+  /**
+   * Boolean to check if new recording is allowed
+   */
+  isNewRecordingAllowed: boolean = false;
+
+  /**
+   * Boolean to check if recording controls are visible
+   */
+  isRecordingControlsVisible: boolean = false;
+
+  /**
+   * Boolean to check if alarm is enabled
+   */
+  isAlarmEnabled: boolean = false;
+
+  /**
+   * @description This method toggles the recording status. If the passed index is 0 and
+   * no new recording is currently allowed (isNewRecordingAllowed is false),
+   * it toggles the isRecording status.
+   * If recording starts, the method changes the first element of liveTitles
+   * to 'Recording...' and hides the recording controls.
+   * If recording ends, it resets the first element of liveTitles, sets
+   * isRecording to false, isNewRecordingAllowed to true,
+   * and makes the recording controls visible.
+   *
+   * @param index - The index of the recording
+   */
+  onRecordToggle(index: number) {
+    if (index === 0 && !this.isNewRecordingAllowed) {
+      this.isRecording = !this.isRecording;
+      if (this.isRecording) {
+        this.originalTitle = this.liveTitles[0];
+        this.liveTitles[0] = 'Recording...';
+        this.isRecordingControlsVisible = false; // set it to false when recording starts
+      } else {
+        this.liveTitles[0] = this.originalTitle;
+        this.isRecording = false; // set it to false when recording ends
+        this.isNewRecordingAllowed = true;
+        this.isRecordingControlsVisible = true; // set it to true when recording ends
+      }
+    }
+  }
+
+  /**
+   * @description This method plays the recording. If the passed index is 0,
+   * @param index - The index of the card
+   */
+  onPlay(index: number) {
+    if (index === 0) {
+      // Your code to play the recording...
+      //
+      console.log('Recording playing!');
+    }
+  }
+
+  /**
+   * @description This method sends the recording. If the passed index is 0,
+   * @param index - The index of the card
+   */
+  onSend(index: number) {
+    if (index === 0) {
+      // Your code to send the recording...
+      //
+      console.log('Recording sent!');
+      this.resetRecordingState();
+    }
+  }
+
+  /**
+   * @description This method deletes the recording. If the passed index is 0,
+   * @param index - The index of the recording
+   */
+  onDelete(index: number) {
+    if (index === 0) {
+      // Your code to delete the recording...
+      //
+      console.log('Recording deleted!');
+      this.resetRecordingState();
+    }
+  }
+
+  /**
+   * @description This method resets the recording state.
+   */
+  resetRecordingState() {
+    this.isNewRecordingAllowed = false;
+    this.isRecordingControlsVisible = false;
+  }
+
+  /**
+   *
+   * @param index - The index of the card
+   */
+  onAlarmToggle(index: number) {
+    if (index === 2) {
+      this.isAlarmEnabled = !this.isAlarmEnabled;
+
+      if (this.isAlarmEnabled) {
+        // Your code to trigger the alarm on backend...
+        //
+        console.log('Alarm triggered!');
+      }
+    }
+  }
 }
