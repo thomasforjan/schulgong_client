82f5dc422f6423c958f6d34edcdca093c3bf271d
Thomas Forjan <112717699+thomasforjan@users.noreply.github.com>
2023-05-19 11:56:52 +0200

    fix: Make holiday.component.spec.ts test runnable
    Updated holiday.component.spec.ts
    - It is now necessary to give a button a click event to prevent propagation.
    
    SCHUL-27

--- src/app/pages/holiday/holiday.component.spec.ts
+++ src/app/pages/holiday/holiday.component.spec.ts
@@ -1,26 +1,24 @@
-import {ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
+import { ComponentFixture, fakeAsync, TestBed } from '@angular/core/testing';
 
-import {HolidayComponent} from './holiday.component';
-import {HttpClientTestingModule} from "@angular/common/http/testing";
-import {HeroImageComponent} from "../../components/hero-image/hero-image.component";
-import {ButtonComponent} from "../../components/button/button.component";
-import {GridCardsComponent} from "../../components/grid-cards/grid-cards.component";
-import {Holiday} from "../../models/Holiday";
+import { HolidayComponent } from './holiday.component';
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { HeroImageComponent } from '../../components/hero-image/hero-image.component';
+import { ButtonComponent } from '../../components/button/button.component';
+import { GridCardsComponent } from '../../components/grid-cards/grid-cards.component';
+import { Holiday } from '../../models/Holiday';
 
-
-import {of} from "rxjs";
-import {RouterTestingModule} from "@angular/router/testing";
-import {MaterialModule} from "../../material.module";
-import {HarnessLoader} from "@angular/cdk/testing";
-import {TestbedHarnessEnvironment} from "@angular/cdk/testing/testbed";
-import {MatCardHarness, MatCardSection} from "@angular/material/card/testing";
-import {findComponent} from '../../spec-helpers/element.spec-helper';
-import {MatDialogHarness} from "@angular/material/dialog/testing";
-import {AddEditHolidaysComponent} from "./add-edit-holidays/add-edit-holidays.component";
-import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
-import {By} from "@angular/platform-browser";
-import {HolidayBackendService} from "../../services/holiday.backend.service";
-
+import { of } from 'rxjs';
+import { RouterTestingModule } from '@angular/router/testing';
+import { MaterialModule } from '../../material.module';
+import { HarnessLoader } from '@angular/cdk/testing';
+import { TestbedHarnessEnvironment } from '@angular/cdk/testing/testbed';
+import { MatCardHarness, MatCardSection } from '@angular/material/card/testing';
+import { findComponent } from '../../spec-helpers/element.spec-helper';
+import { MatDialogHarness } from '@angular/material/dialog/testing';
+import { AddEditHolidaysComponent } from './add-edit-holidays/add-edit-holidays.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { By } from '@angular/platform-browser';
+import { HolidayBackendService } from '../../services/holiday.backend.service';
 
 /**
  * @author: Thomas Forjan, Philipp Wildzeiss, Martin Kral
@@ -38,39 +36,56 @@ describe('HolidayComponent', () => {
   let loader: HarnessLoader;
 
   beforeEach(async () => {
-
-    testHoliday = [{
-      id: 1,
-      name: "Sommerferien",
-      startDate: new Date(2023, 7, 5),
-      endDate: new Date(2023, 9, 1)
-    },
+    testHoliday = [
+      {
+        id: 1,
+        name: 'Sommerferien',
+        startDate: new Date(2023, 7, 5),
+        endDate: new Date(2023, 9, 1),
+      },
       {
         id: 2,
-        name: "Semesterferien",
+        name: 'Semesterferien',
         startDate: new Date(2023, 2, 5),
-        endDate: new Date(2023, 2, 13)
-      }
+        endDate: new Date(2023, 2, 13),
+      },
     ];
 
     // Create a fake TwainService object with a `getQuote()` spy
-    const holidayBackendService = jasmine.createSpyObj('HolidayBackendService', ['getHolidayResponse']);
+    const holidayBackendService = jasmine.createSpyObj(
+      'HolidayBackendService',
+      ['getHolidayResponse']
+    );
     // Make the spy return a synchronous Observable with the test data
-    getHolidaySpy = holidayBackendService.getHolidayResponse.and.returnValue(of(testHoliday));
+    getHolidaySpy = holidayBackendService.getHolidayResponse.and.returnValue(
+      of(testHoliday)
+    );
 
     await TestBed.configureTestingModule({
-      declarations: [HolidayComponent, HeroImageComponent, ButtonComponent, GridCardsComponent, AddEditHolidaysComponent],
+      declarations: [
+        HolidayComponent,
+        HeroImageComponent,
+        ButtonComponent,
+        GridCardsComponent,
+        AddEditHolidaysComponent,
+      ],
       // imports: [HttpClientTestingModule, MatDialogModule, MatSnackBarModule, MatTooltipModule, MatIconModule, MatCardModule, RouterTestingModule],
-      imports: [HttpClientTestingModule, MaterialModule, RouterTestingModule, BrowserAnimationsModule],
-      providers: [{provide: HolidayBackendService, useValue: holidayBackendService}]
+      imports: [
+        HttpClientTestingModule,
+        MaterialModule,
+        RouterTestingModule,
+        BrowserAnimationsModule,
+      ],
+      providers: [
+        { provide: HolidayBackendService, useValue: holidayBackendService },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(HolidayComponent);
     component = fixture.componentInstance;
-    fixture.detectChanges();  // onInit()
+    fixture.detectChanges(); // onInit()
     loader = TestbedHarnessEnvironment.documentRootLoader(fixture);
-    holidayUploadBtn = fixture.nativeElement.querySelector('.uploadBtn')
-
+    holidayUploadBtn = fixture.nativeElement.querySelector('.uploadBtn');
   });
   describe('holiday overview test', () => {
     it('check getHolidayResponse called', async () => {
@@ -80,20 +95,29 @@ describe('HolidayComponent', () => {
     });
 
     it('check card-header exist', async () => {
-      const card = await loader.getHarness(MatCardHarness.with({title: testHoliday[0].name}));
-      const footerSubcomponents = (await card.getAllChildLoaders(MatCardSection.HEADER)) ?? [];
+      const card = await loader.getHarness(
+        MatCardHarness.with({ title: testHoliday[0].name })
+      );
+      const footerSubcomponents =
+        (await card.getAllChildLoaders(MatCardSection.HEADER)) ?? [];
       expect(footerSubcomponents.length).toBe(1);
     });
 
     it('check card-content exist', async () => {
-      const card = await loader.getHarness(MatCardHarness.with({title: testHoliday[0].name}));
-      const contentSubcomponents = (await card.getAllChildLoaders(MatCardSection.CONTENT)) ?? [];
+      const card = await loader.getHarness(
+        MatCardHarness.with({ title: testHoliday[0].name })
+      );
+      const contentSubcomponents =
+        (await card.getAllChildLoaders(MatCardSection.CONTENT)) ?? [];
       expect(contentSubcomponents.length).toBe(1);
     });
 
     it('check card-action exist', async () => {
-      const card = await loader.getHarness(MatCardHarness.with({title: testHoliday[0].name}));
-      const footerSubcomponents = (await card.getAllChildLoaders(MatCardSection.ACTIONS)) ?? [];
+      const card = await loader.getHarness(
+        MatCardHarness.with({ title: testHoliday[0].name })
+      );
+      const footerSubcomponents =
+        (await card.getAllChildLoaders(MatCardSection.ACTIONS)) ?? [];
       expect(footerSubcomponents.length).toBe(1);
     });
 
@@ -108,7 +132,7 @@ describe('HolidayComponent', () => {
     it('click upload button and check if dialog exist', fakeAsync(async () => {
       holidayUploadBtn.click();
       // sync spy result shows testQuote immediately after init
-      fixture.detectChanges();  // onInit()
+      fixture.detectChanges(); // onInit()
       const dialog = await loader.getAllHarnesses(MatDialogHarness); // fails here
       expect(dialog.length).toBe(1);
     }));
@@ -118,9 +142,9 @@ describe('HolidayComponent', () => {
         document.getElementsByTagName('mat-card')
       );
       let i = 0;
-      ourDomCardsUnderTest.forEach(card => {
-        const cardTitle = card.getElementsByTagName('mat-card-title')[0]
-          .textContent;
+      ourDomCardsUnderTest.forEach((card) => {
+        const cardTitle =
+          card.getElementsByTagName('mat-card-title')[0].textContent;
         expect(cardTitle).toBe(testHoliday[i].name);
         i++;
       });
@@ -131,19 +155,23 @@ describe('HolidayComponent', () => {
         document.getElementsByTagName('mat-card')
       );
       let i = 0;
-      ourDomCardsUnderTest.forEach(card => {
-        const cardContent = card.getElementsByTagName('mat-card-content')[0]
-          .textContent;
+      ourDomCardsUnderTest.forEach((card) => {
+        const cardContent =
+          card.getElementsByTagName('mat-card-content')[0].textContent;
         if (cardContent != null) {
-          expect(cardContent.trim()).toBe(testHoliday[i].startDate.toLocaleDateString('de-DE', {
-            day: '2-digit',
-            month: '2-digit',
-            year: 'numeric'
-          }) + " - " + testHoliday[i].endDate.toLocaleDateString('de-DE', {
-            day: '2-digit',
-            month: '2-digit',
-            year: 'numeric'
-          }));
+          expect(cardContent.trim()).toBe(
+            testHoliday[i].startDate.toLocaleDateString('de-DE', {
+              day: '2-digit',
+              month: '2-digit',
+              year: 'numeric',
+            }) +
+              ' - ' +
+              testHoliday[i].endDate.toLocaleDateString('de-DE', {
+                day: '2-digit',
+                month: '2-digit',
+                year: 'numeric',
+              })
+          );
         }
         i++;
       });
@@ -157,9 +185,12 @@ describe('HolidayComponent', () => {
     it('check correct cards button exist', fakeAsync(async () => {
       const el = findComponent(fixture, 'app-grid-cards');
       // First get the DebugElement
-      const gridCardDebugEl = fixture.debugElement.query(By.directive(GridCardsComponent));
+      const gridCardDebugEl = fixture.debugElement.query(
+        By.directive(GridCardsComponent)
+      );
       // Then get the component
-      const gridCardssComponent = gridCardDebugEl.injector.get(GridCardsComponent);
+      const gridCardssComponent =
+        gridCardDebugEl.injector.get(GridCardsComponent);
       expect(gridCardssComponent.showEditButton).toBe(true);
       expect(gridCardssComponent.showPlayButton).toBe(false);
       expect(gridCardssComponent.showDeleteButton).toBe(true);
@@ -168,24 +199,25 @@ describe('HolidayComponent', () => {
     it('check onclick delete button', fakeAsync(async () => {
       const el = findComponent(fixture, 'app-grid-cards');
       // First get the DebugElement
-      const gridCardDebugEl = fixture.debugElement.query(By.directive(GridCardsComponent));
+      const gridCardDebugEl = fixture.debugElement.query(
+        By.directive(GridCardsComponent)
+      );
       // Then get the component
-      const gridCardssComponent = gridCardDebugEl.injector.get(GridCardsComponent);
+      const gridCardssComponent =
+        gridCardDebugEl.injector.get(GridCardsComponent);
+      // Create a new Event instance
+      const event = new Event('click');
+
       let i = 1;
-      testHoliday.forEach(holiday => {
-        gridCardssComponent.onDelete(testHoliday[0].id);
+      testHoliday.forEach((holiday) => {
+        gridCardssComponent.onDelete(testHoliday[0].id, event);
       });
       const dialog = await loader.getAllHarnesses(MatDialogHarness); // fails here
       expect(dialog.length).toBe(testHoliday.length);
     }));
-
-
   });
-
 
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
-
-
