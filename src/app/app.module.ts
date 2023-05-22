087a292830a034fa0b625ff62246305627739718
Thomas Forjan <112717699+thomasforjan@users.noreply.github.com>
2023-05-22 19:39:58 +0200

    feat: Added FullCalendar dependencies and import in app.module.ts
    Installed the following dependencies for FullCalendar:
    
    "@fullcalendar/angular": "^6.1.7"
    "@fullcalendar/core": "^6.1.7"
    "@fullcalendar/daygrid": "^6.1.7"
    "@fullcalendar/interaction": "^6.1.7"
    "@fullcalendar/list": "^6.1.7"
    "@fullcalendar/timegrid": "^6.1.7"
    "fullcalendar": "^6.1.7"
    
    Also added the FullCalendar module import in app.module.ts.
    
    This commit adds the necessary dependencies and imports for integrating FullCalendar into the project.
    
    SCHUL-31

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,39 +1,43 @@
-import {NgModule} from '@angular/core';
-import {BrowserModule} from '@angular/platform-browser';
-import {AppRoutingModule} from './app-routing.module';
-import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {HttpClientModule} from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AppRoutingModule } from './app-routing.module';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { HttpClientModule } from '@angular/common/http';
 
 /**
  * Material Module
  */
-import {MaterialModule} from './material.module';
+import { MaterialModule } from './material.module';
+
+/**
+ * Full Calendar Module
+ */
+import { FullCalendarModule } from '@fullcalendar/angular';
 
 /**
  * Components
  */
-import {AppComponent} from './app.component';
-import {GridCardsComponent} from './components/grid-cards/grid-cards.component';
-import {FooterComponent} from './layout/footer/footer.component';
-import {HeaderComponent} from './layout/header/header.component';
-import {SidebarComponent} from './layout/sidebar/sidebar.component';
-import {InfoBarComponent} from './components/info-bar/info-bar.component';
-import {ButtonComponent} from './components/button/button.component';
-import {HeroImageComponent} from './components/hero-image/hero-image.component';
-import {DashboardComponent} from './pages/dashboard/dashboard.component';
-import {RingtonesComponent} from './pages/ringtones/ringtones.component';
-import {RingtimeComponent} from './pages/ringtime/ringtime.component';
-import {LiveComponent} from './pages/live/live.component';
-import {HolidayComponent} from './pages/holiday/holiday.component';
-import {CalendarComponent} from './pages/calendar/calendar.component';
-import {UserComponent} from './pages/user/user.component';
-import {AddEditRingtonesComponent} from "./pages/ringtones/add-edit-ringtones/add-edit-ringtones.component";
-import {DeleteDialogComponent} from "./components/delete-dialog/delete-dialog.component";
-import {AddEditRingtimeComponent} from './pages/ringtime/add-edit-ringtime/add-edit-ringtime.component';
-import {MAT_DATE_LOCALE} from '@angular/material/core';
-import {AddEditHolidaysComponent} from "./pages/holiday/add-edit-holidays/add-edit-holidays.component";
+import { AppComponent } from './app.component';
+import { GridCardsComponent } from './components/grid-cards/grid-cards.component';
+import { FooterComponent } from './layout/footer/footer.component';
+import { HeaderComponent } from './layout/header/header.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { InfoBarComponent } from './components/info-bar/info-bar.component';
+import { ButtonComponent } from './components/button/button.component';
+import { HeroImageComponent } from './components/hero-image/hero-image.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { RingtonesComponent } from './pages/ringtones/ringtones.component';
+import { RingtimeComponent } from './pages/ringtime/ringtime.component';
+import { LiveComponent } from './pages/live/live.component';
+import { HolidayComponent } from './pages/holiday/holiday.component';
+import { CalendarComponent } from './pages/calendar/calendar.component';
+import { UserComponent } from './pages/user/user.component';
+import { AddEditRingtonesComponent } from './pages/ringtones/add-edit-ringtones/add-edit-ringtones.component';
+import { DeleteDialogComponent } from './components/delete-dialog/delete-dialog.component';
+import { AddEditRingtimeComponent } from './pages/ringtime/add-edit-ringtime/add-edit-ringtime.component';
+import { MAT_DATE_LOCALE } from '@angular/material/core';
+import { AddEditHolidaysComponent } from './pages/holiday/add-edit-holidays/add-edit-holidays.component';
 import { MusicComponent } from './pages/live/music/music.component';
-
 
 /**
  * @author: Thomas Forjan, Philipp Wildzeiss, Martin Kral
@@ -62,7 +66,7 @@ import { MusicComponent } from './pages/live/music/music.component';
     DeleteDialogComponent,
     AddEditRingtimeComponent,
     AddEditHolidaysComponent,
-    MusicComponent
+    MusicComponent,
   ],
   imports: [
     BrowserModule,
@@ -70,9 +74,9 @@ import { MusicComponent } from './pages/live/music/music.component';
     BrowserAnimationsModule,
     MaterialModule,
     HttpClientModule,
+    FullCalendarModule,
   ],
-  providers: [{provide: MAT_DATE_LOCALE, useValue: 'de-DE'}],
+  providers: [{ provide: MAT_DATE_LOCALE, useValue: 'de-DE' }],
   bootstrap: [AppComponent],
 })
-export class AppModule {
-}
+export class AppModule {}
