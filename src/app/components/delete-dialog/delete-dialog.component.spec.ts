6f7d2c8fe397cebb93fd2199d0569af250224e50
Thomas Forjan <112717699+thomasforjan@users.noreply.github.com>
2023-05-30 23:04:30 +0200

    test: Add delete-dialog component Karma test
    This commit adds a Karma test suite for the DeleteDialogComponent. The test suite includes two tests to ensure the proper functionality of the component.
    
    The test suite covers the following scenarios:
    
    Checking if the component is created successfully
    Verifying if the onCancelClick method closes the dialog without any data
    Verifying if the onSubmitClick method closes the dialog with the provided data
    
    The necessary test bed setup is performed, including the configuration of required modules, providers, and component declarations. Additionally, a spy object for MatDialogRef is created to simulate the close method of the dialog.
    
    The tests validate the behavior of the DeleteDialogComponent by calling the corresponding methods and checking if the expected actions are performed. This includes closing the dialog without any data on cancel and closing the dialog with the provided data on submit.
    
    These tests ensure that the DeleteDialogComponent functions correctly and adheres to its intended behavior.
    
    SCHUL-160

--- src/app/components/delete-dialog/delete-dialog.component.spec.ts
+++ src/app/components/delete-dialog/delete-dialog.component.spec.ts
@@ -1,36 +1,78 @@
-import {ComponentFixture, TestBed} from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 
-import {DeleteDialogComponent} from './delete-dialog.component';
-import {HeroImageComponent} from "../hero-image/hero-image.component";
-import {ButtonComponent} from "../button/button.component";
-import {MatTooltipModule} from "@angular/material/tooltip";
-import {MatIconModule} from "@angular/material/icon";
-import {MAT_DIALOG_DATA, MatDialogRef} from "@angular/material/dialog";
+import { DeleteDialogComponent } from './delete-dialog.component';
+import { HeroImageComponent } from '../hero-image/hero-image.component';
+import { ButtonComponent } from '../button/button.component';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { MatIconModule } from '@angular/material/icon';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
 
 /**
  * @author: Thomas Forjan, Philipp Wildzeiss, Martin Kral
- * @version: 0.0.2
+ * @version: 0.0.3
  * @since: April 2023
  * @description: Tests for reusable delete dialog
+ */
+
+/**
+ * @description
+ * Suite of tests for the DeleteDialogComponent
  */
 describe('DeleteDialogComponent', () => {
   let component: DeleteDialogComponent;
   let fixture: ComponentFixture<DeleteDialogComponent>;
+  let mockDialogRef: jasmine.SpyObj<MatDialogRef<DeleteDialogComponent>>;
 
+  /**
+   * @description
+   * Set up the test bed with the necessary modules, providers and component declarations.
+   * Create a spy object for MatDialogRef with a `close` method.
+   */
   beforeEach(async () => {
+    mockDialogRef = jasmine.createSpyObj('MatDialogRef', ['close']);
     await TestBed.configureTestingModule({
-      declarations: [DeleteDialogComponent, HeroImageComponent, ButtonComponent ],
+      declarations: [
+        DeleteDialogComponent,
+        HeroImageComponent,
+        ButtonComponent,
+      ],
       imports: [MatTooltipModule, MatIconModule],
-      providers: [{provide: MatDialogRef, useValue: {}}, {provide: MAT_DIALOG_DATA, useValue: {}}]
-    })
-      .compileComponents();
+      providers: [
+        { provide: MatDialogRef, useValue: mockDialogRef },
+        { provide: MAT_DIALOG_DATA, useValue: { deleteItem: {}, index: 0 } },
+      ],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(DeleteDialogComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
+  /**
+   * @description
+   * Test to check if the component is created correctly.
+   */
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  /**
+   * @description
+   * Test to check if the `onCancelClick` method closes the dialog without any data.
+   */
+  it('should close dialog with no data on cancel', () => {
+    component.onCancelClick();
+    expect(mockDialogRef.close).toHaveBeenCalledTimes(1);
+    expect(mockDialogRef.close).toHaveBeenCalledWith();
+  });
+
+  /**
+   * @description
+   * Test to check if the `onSubmitClick` method closes the dialog with the provided data.
+   */
+  it('should close dialog with data on submit', () => {
+    component.onSubmitClick();
+    expect(mockDialogRef.close).toHaveBeenCalledTimes(1);
+    expect(mockDialogRef.close).toHaveBeenCalledWith(component.data);
   });
 });
