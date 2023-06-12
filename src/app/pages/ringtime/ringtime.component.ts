61c1055f1163cea92cea730c0626af637ffc1e73
Martin Kral <2110859031@fh-burgenland.at>
2023-06-12 16:26:32 +0200

    add: Text to buttons in ringtime component

--- src/app/pages/ringtime/ringtime.component.ts
+++ src/app/pages/ringtime/ringtime.component.ts
@@ -1,6 +1,6 @@
 import {Component, OnInit} from '@angular/core';
 import {map, take} from 'rxjs';
-import {HeroImages, StoreService,} from 'src/app/services/store.service';
+import {ButtonValue, ButtonWidths, HeroImages, StoreService,} from 'src/app/services/store.service';
 import {Ringtime, RingtimeDialog, RingtimePayload,} from '../../models/Ringtime';
 import {AddEditRingtimeComponent} from './add-edit-ringtime/add-edit-ringtime.component';
 import {MatDialog} from '@angular/material/dialog';
@@ -23,11 +23,14 @@ import {RingtoneBackendService} from "../../services/ringtone.backend.service";
   templateUrl: './ringtime.component.html',
   styleUrls: ['./ringtime.component.scss'],
 })
-export class RingtimeComponent implements OnInit{
+export class RingtimeComponent implements OnInit {
   /**
    * Ringtime Hero Image from enum in store service
    */
   ringtimeHeroImage: string = HeroImages.RingtimeHeroImage;
+
+  protected readonly ButtonValue = ButtonValue;
+  protected readonly ButtonWidths = ButtonWidths;
 
   /**
    * Get the length of the ringtime list
